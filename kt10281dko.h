9d956d528805b1269529833548d9e4ccf034a025
miracle-beep <76535377+miracle-beep@users.noreply.github.com>
2024-01-03 22:22:48 +0300

    Add files via upload

--- kt10281dko.h
+++ kt10281dko.h
@@ -1,9 +1,19 @@
 #define DISPLAY PORTD //Display port
-unsigned int numbers[10] = {0xDE,0x0A,0xEC,0x6E,0x3A,0x76,0xF6,0x0E,0xFE,0x7E}; //0123456789
-unsigned int letters[7] = {0xBE, 0xF2, 0xD4, 0xEA, 0xF4, 0xB4,0xBA}; //abcdefhijl
-void display_number(unsigned int number){
-	DISPLAY = numbers[number];
+unsigned int digits[22] = {0xDE,0x0A,0xEC,0x6E,0x3A,0x76,0xF6,0x0E,0xFE,0x7E, 0xBE, 0xF2, 0xD4, 0xEA, 0xF4, 0xB4,0xBA,0x90,0x4A,0xD0,0xA2,0xE2}; //0123456789abcdefhijlno
+void display_digit(unsigned int number){
+	DISPLAY = digits[number];
 }
-void display_letter(unsigned int number){
-	DISPLAY = letters[number];
+void clean_display(){
+	DISPLAY = 0;
+}	
+int letter_to_number(char symbol){
+	char mask[22]="0123456789abcdefhijlno";
+	for(int i=0; i<22; i++){
+		if(mask[i]==symbol){
+			return i;
+		}
+	}
 }
+
+
+
--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <kt10281dko.h>
+#define SLEEPTIME 1000
 #define F_CPU 1000000UL
 #define KEY PORTB
-#define SLEEPTIME 500
 int main(void){
 	DDRD = 0b11111111;
 	DDRB = 0b00000000;
 	DISPLAY = 0b00000000;
 	KEY = 0b11111111;
 	while(1){
-		unsigned int ctr = 0;
-		unsigned int ctr_let = 0;
-		 if(PINB == 0b11111111){
-		 for(ctr;ctr<10;ctr++){
-			 display_number(ctr);
-			 _delay_ms(SLEEPTIME);
-		 }
-	 }
-		 else{
-		 for(ctr_let; ctr_let<7;ctr_let++){
-			 display_letter(ctr_let);
-			 _delay_ms(SLEEPTIME);
-		 }
+		display_digit(letter_to_number('h'));
+		_delay_ms(SLEEPTIME);
+		clean_display();
+		display_digit(letter_to_number('e'));
+		_delay_ms(SLEEPTIME);
+		clean_display();
+		display_digit(letter_to_number('l'));
+		_delay_ms(SLEEPTIME);
+		clean_display();
+		display_digit(letter_to_number('l'));
+		_delay_ms(SLEEPTIME);
+		clean_display();
+		display_digit(letter_to_number('o'));
+		_delay_ms(SLEEPTIME);
+		clean_display();
 		 }
 	}
-}
